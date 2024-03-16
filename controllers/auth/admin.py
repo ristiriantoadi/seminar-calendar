dacdabba1c461556b34d9ff03f088a655ca8d4b0
Ristirianto Adi <ristiriantoadi@gmail.com>
2024-03-16 14:34:19 +0700

    login by email or nip

--- controllers/auth/admin.py
+++ controllers/auth/admin.py
@@ -10,7 +10,9 @@ from models.user.admin import OutputAdmin
 
 
 async def authenticate_admin(username: str, password: str) -> OutputAdmin:
-    admin = await find_admin_on_db(criteria={"nip": username.strip()})
+    admin = await find_admin_on_db(
+        criteria={"$or": [{"nip": username}, {"email": username}]}
+    )
     if admin is None:
         raise HTTPException(status_code=401, detail="Username atau password salah")
     if not PWDCONTEXT.verify(password.strip(), admin.credential.password):
